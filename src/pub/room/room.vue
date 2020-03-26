3a5e79a8211beb6aea61e77c958c9cfc27217327
mynameisliyao <3113136208@qq.com>
2020-03-26 22:08:56 +0800

    客房修改和客房类型修改

--- src/pub/room/room.vue
+++ src/pub/room/room.vue
@@ -117,7 +117,7 @@
         direction="rtl"
         size="50%">
           <div v-for="(img, index) in roomData.imgList" :key="index">
-            <img :src="img" alt="图片暂时无法找到">
+            <img :src="img" width="400" alt="图片暂时无法找到">
           </div>
       </el-drawer>
     </el-table>
@@ -325,6 +325,9 @@ export default {
 </script>
 
 <style lang="less">
+.el-drawer__body {
+  overflow: auto;
+}
 #room{
   // .el-row {
   //   margin-bottom: 10px;
